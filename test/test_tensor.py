f3092295dfce9e067af31f1acc956aac213623f5
Cem Alpturk <cem.alpturk@gmail.com>
2024-08-19 10:42:21 +0200

    unfinished tensor work

---
+++ test/test_tensor.py
@@ -0,0 +1,269 @@
+import numpy as np
+from torchlet.tensor import Tensor
+
+
+def test_tensor_init():
+    array = np.array([1, 2, 3])
+    tensor = Tensor(array)
+    assert np.array_equal(tensor.array, array)
+    assert tensor.requires_grad
+    assert tensor.dtype == np.float32
+    assert tensor.name is None
+    assert tensor.args == ()
+    assert tensor._id is not None
+
+
+def test_tensor_backward():
+    array = np.array([1, 2, 3])
+    tensor = Tensor(array)
+    tensor.backward()
+    assert np.array_equal(tensor.grad.array, np.ones_like(array))
+
+
+def test_tensor_shape():
+    array = np.array([[1, 2], [3, 4]])
+    tensor = Tensor(array)
+    assert tensor.shape == (2, 2)
+
+
+def test_tensor_ndim():
+    array = np.array([1, 2, 3])
+    tensor = Tensor(array)
+    assert tensor.ndim == 1
+
+
+def test_tensor_transpose():
+    array = np.array([[1, 2], [3, 4]])
+    tensor = Tensor(array)
+    transposed = tensor.transpose()
+    expected_array = np.array([[1, 3], [2, 4]])
+    assert np.array_equal(transposed.array, expected_array)
+
+
+def test_tensor_dot():
+    array1 = np.array([[1, 2], [3, 4]])
+    array2 = np.array([[5, 6], [7, 8]])
+    tensor1 = Tensor(array1)
+    tensor2 = Tensor(array2)
+    dot_product = tensor1.dot(tensor2)
+    expected_array = array1.dot(array2)
+    assert np.array_equal(dot_product.array, expected_array)
+
+
+def test_tensor_relu():
+    array = np.array([-1, 2, -3])
+    tensor = Tensor(array)
+    relu = tensor.relu()
+    expected_array = np.array([0, 2, 0])
+    assert np.array_equal(relu.array, expected_array)
+
+
+def test_tensor_add():
+    array1 = np.array([1, 2, 3])
+    array2 = np.array([4, 5, 6])
+    tensor1 = Tensor(array1)
+    tensor2 = Tensor(array2)
+    added = tensor1 + tensor2
+    expected_array = np.array([5, 7, 9])
+    assert np.array_equal(added.array, expected_array)
+
+    added.backward()
+
+    expected = np.array([1, 1, 1])
+    assert np.array_equal(tensor1.grad.array, expected)
+    assert np.array_equal(tensor2.grad.array, expected)
+
+    tensor = Tensor(array1)
+    value = 5
+    added = tensor + value
+    expected_array = np.array([6, 7, 8])
+    assert np.array_equal(added.array, expected_array)
+    added.backward()
+    assert np.array_equal(tensor.grad.array, np.ones_like(array1))
+
+    tensor = Tensor(array1)
+    value = 5
+    added = value + tensor
+    expected_array = np.array([6, 7, 8])
+    assert np.array_equal(added.array, expected_array)
+    added.backward()
+    assert np.array_equal(tensor.grad.array, np.ones_like(array1))
+
+
+def test_tensor_sub():
+    array1 = np.array([4, 5, 6])
+    array2 = np.array([1, 2, 3])
+    tensor1 = Tensor(array1)
+    tensor2 = Tensor(array2)
+    subtracted = tensor1 - tensor2
+    expected_array = np.array([3, 3, 3])
+    assert np.array_equal(subtracted.array, expected_array)
+    subtracted.backward()
+    assert np.array_equal(tensor1.grad.array, np.ones_like(array1))
+    assert np.array_equal(tensor2.grad.array, -np.ones_like(array2))
+
+    tensor = Tensor(array1)
+    value = 2
+    subtracted = tensor - value
+    expected_array = np.array([2, 3, 4])
+    assert np.array_equal(subtracted.array, expected_array)
+    subtracted.backward()
+    assert np.array_equal(tensor.grad.array, np.ones_like(array1))
+
+    tensor = Tensor(array1)
+    value = 2
+    subtracted = value - tensor
+    expected_array = np.array([-2, -3, -4])
+    assert np.array_equal(subtracted.array, expected_array)
+    subtracted.backward()
+    assert np.array_equal(tensor.grad.array, -np.ones_like(array1))
+
+
+def test_tensor_mul():
+    array1 = np.array([1, 2, 3])
+    array2 = np.array([4, 5, 6])
+    tensor1 = Tensor(array1)
+    tensor2 = Tensor(array2)
+    multiplied = tensor1 * tensor2
+    expected_array = np.array([4, 10, 18])
+    assert np.array_equal(multiplied.array, expected_array)
+    multiplied.backward()
+    assert np.array_equal(tensor1.grad.array, array2)
+    assert np.array_equal(tensor2.grad.array, array1)
+
+    tensor = Tensor(array1)
+    value = 2
+    multiplied = tensor * value
+    expected_array = np.array([2, 4, 6])
+    assert np.array_equal(multiplied.array, expected_array)
+    multiplied.backward()
+    assert np.array_equal(tensor.grad.array, value * np.ones_like(array1))
+
+    tensor = Tensor(array1)
+    value = 2
+    multiplied = value * tensor
+    expected_array = np.array([2, 4, 6])
+    assert np.array_equal(multiplied.array, expected_array)
+    multiplied.backward()
+    assert np.array_equal(tensor.grad.array, value * np.ones_like(array1))
+
+
+def test_tensor_pow():
+    array = np.array([2, 3, 4])
+    tensor = Tensor(array)
+    powered = tensor**2
+    expected_array = np.array([4, 9, 16])
+    assert np.array_equal(powered.array, expected_array)
+    powered.backward()
+    assert np.array_equal(tensor.grad.array, 2 * array)
+
+
+def test_tensor_div():
+    array1 = np.array([4, 6, 8])
+    array2 = np.array([2, 3, 4])
+    tensor1 = Tensor(array1)
+    tensor2 = Tensor(array2)
+    divided = tensor1 / tensor2
+    expected_array = np.array([2, 2, 2])
+    assert np.array_equal(divided.array, expected_array)
+    divided.backward()
+    assert np.all(np.isclose(tensor1.grad.array, 1 / array2))
+    assert np.all(np.isclose(tensor2.grad.array, -array1 / array2**2))
+
+    tensor = Tensor(array1)
+    value = 2
+    divided = tensor / value
+    expected_array = np.array([2, 3, 4])
+    assert np.array_equal(divided.array, expected_array)
+    divided.backward()
+    assert np.array_equal(tensor.grad.array, 1 / value * np.ones_like(array1))
+
+    tensor = Tensor(array1)
+    value = 2
+    divided = value / tensor
+    expected_array = np.array([0.5, 1 / 3, 0.25])
+    assert np.all(np.isclose(divided.array, expected_array))
+    divided.backward()
+    assert np.all(np.isclose(tensor.grad.array, -value / array1**2))
+
+
+def test_tensor_comparison():
+    array1 = np.array([1, 2, 3])
+    array2 = np.array([2, 2, 2])
+    tensor1 = Tensor(array1)
+    tensor2 = Tensor(array2)
+    less_than = tensor1 < tensor2
+    expected_array = np.array([True, False, False])
+    assert np.array_equal(less_than.array, expected_array)
+
+    less_than_or_equal = tensor1 <= tensor2
+    expected_array = np.array([True, True, False])
+    assert np.array_equal(less_than_or_equal.array, expected_array)
+
+    equal = tensor1 == tensor2
+    expected_array = np.array([False, True, False])
+    assert np.array_equal(equal.array, expected_array)
+
+    not_equal = tensor1 != tensor2
+    expected_array = np.array([True, False, True])
+    assert np.array_equal(not_equal.array, expected_array)
+
+    greater_than = tensor1 > tensor2
+    expected_array = np.array([False, False, True])
+    assert np.array_equal(greater_than.array, expected_array)
+
+    greater_than_or_equal = tensor1 >= tensor2
+    expected_array = np.array([False, True, True])
+    assert np.array_equal(greater_than_or_equal.array, expected_array)
+
+
+def test_tensor_getitem():
+    array = np.array([1, 2, 3, 4, 5])
+    tensor = Tensor(array)
+    sliced = tensor[1:4]
+    expected_array = np.array([2, 3, 4])
+    assert np.array_equal(sliced.array, expected_array)
+
+
+def test_tensor_setitem():
+    array = np.array([1, 2, 3, 4, 5])
+    tensor = Tensor(array)
+    tensor[1:4] = 0
+    expected_array = np.array([1, 0, 0, 0, 5])
+    assert np.array_equal(tensor.array, expected_array)
+
+
+def test_tensor_matmul():
+    array1 = np.array([[1, 2], [3, 4]])
+    array2 = np.array([[5, 6], [7, 8]])
+    tensor1 = Tensor(array1)
+    tensor2 = Tensor(array2)
+    matmul = tensor1 @ tensor2
+    expected_array = array1 @ array2
+    assert np.array_equal(matmul.array, expected_array)
+    matmul.backward()
+    print(tensor1.grad.array)
+    assert np.array_equal(tensor1.grad.array, np.ones_like(expected_array) @ array2.T)
+    assert np.array_equal(tensor2.grad.array, array1.T @ np.ones_like(expected_array))
+
+
+def test_numerical_grad():
+    def f(x: Tensor) -> Tensor:
+        return x**2 + 2 * x
+
+    x1 = Tensor(10.0, dtype=np.float64)
+    y1 = f(x1)
+
+    y1.backward()
+
+    x_grad = x1.grad.array
+
+    # Numerical gradient
+    epsilon = 1e-6
+    x2 = Tensor(10.0 + epsilon)
+    y2 = f(x2)
+    print(y1.array)
+    print(y2.array)
+    numerical_grad = (y2.array - y1.array) / epsilon
+
+    assert np.isclose(x_grad, numerical_grad, atol=1e-3)
--- torchlet/tensor.py
+++ torchlet/tensor.py
@@ -1,14 +1,15 @@
-import numbers
+from typing import Union, Callable, Sequence, TypeAlias
 import uuid
 import logging
 
 import numpy as np
-import numpy.typing
-from numpy.typing import ArrayLike
+from numpy.typing import DTypeLike, ArrayLike
 
 logger = logging.getLogger(__name__)
 
 DEFAULT_TYPE = np.float32
+
+INP_TYPE: TypeAlias = Union[float, int, "Tensor"]
 
 
 class Tensor:
@@ -20,192 +21,317 @@ class Tensor:
         self,
         array: ArrayLike,
         requires_grad: bool = True,
-        args: tuple["Tensor", ...] | None = None,
-        dtype: np.typing.DTypeLike | None = None,
+        dtype: DTypeLike | None = None,
         name: str | None = None,
+        args: tuple["Tensor", ...] | None = None,
+        backward_fn: Callable[["Tensor"], None] | None = None,
         _id: int | None = None,
     ) -> None:
         """
         Initialize a new Tensor object.
         TODO: docstring
         """
-
+        # Needed?
         if isinstance(array, Tensor):
             self = array
             return
 
-        self._id = _id or uuid.uuid4().int
-        self._parents = None
+        if np.isscalar(array):
+            self.array = np.array([array])
+        else:
+            self.array = np.array(array)
 
-        self.array = np.array(array)
-
+        # Converting to default dtype is this desired behavior?
         if dtype is None:
             dtype = DEFAULT_TYPE
 
-        self.array = self.array.astype(dtype)
-        self.grad = None
-
+        #  self.array = self.array.astype(dtype)
         self.requires_grad = requires_grad
-        self.args = args
 
-        self._backward = lambda: None
         self.name = name
+        self._id = _id or uuid.uuid1().int
+        self.args = args or tuple()
+
+        def _backward(dy: "Tensor") -> None:
+            return None
+
+        if self.requires_grad:
+            self._backward = backward_fn if backward_fn is not None else _backward
+            self.grad = Tensor(np.zeros_like(self.array), requires_grad=False)
 
     def backward(self) -> None:
         """
         Compute the gradient of the tensor.
+        TODO: implement
         """
-        pass
+        topo: list["Tensor"] = []
+        visited = set()
+
+        def build_topo(v: "Tensor") -> None:
+            if v not in visited:
+                visited.add(v)
+                for child in v.args:
+                    build_topo(child)
+                topo.append(v)
+
+        build_topo(self)
+
+        self.grad = Tensor(np.ones_like(self.array), requires_grad=False)
+        for v in reversed(topo):
+            v._backward(v.grad)
+
+    @property
+    def shape(self) -> Sequence[int]:
+        return self.array.shape
+
+    @property
+    def ndim(self) -> int:
+        return self.array.ndim
+
+    @property
+    def dtype(self) -> np.dtype:
+        return self.array.dtype
+
+    @property
+    def T(self) -> "Tensor":
+        return self.transpose()
+
+    def reshape(self, shape: Sequence[int]) -> "Tensor":
+        # TODO: Implement
+        raise NotImplementedError
+
+    def sum(self) -> "Tensor":
+        def _backward(dy: "Tensor") -> None:
+            self.grad += Tensor(np.ones_like(self.array)) * dy
+
+        out = Tensor(
+            array=np.sum(self.array),
+            args=(self,),
+            name="sum",
+            backward_fn=_backward,
+        )
+
+        return out
+
+    def mean(self) -> "Tensor":
+        def _backward(dy: "Tensor") -> None:
+            grad_divisor = self.array.size
+            self.grad += dy / grad_divisor  # type: ignore
+
+        out = Tensor(
+            array=np.mean(self.array),
+            args=(self,),
+            name="mean",
+            backward_fn=_backward,
+        )
+
+        return out
+
+    def transpose(self) -> "Tensor":
+        def _backward(dy: "Tensor") -> None:
+            self.grad += dy.transpose()
+
+        out = Tensor(
+            array=self.array.T,
+            args=(self,),
+            name="transpose",
+            backward_fn=_backward,
+        )
+
+        return out
+
+    def dot(self, other: "Tensor") -> "Tensor":
+        def _backward(dy: "Tensor") -> None:
+            self.grad += dy.dot(other.T)
+            other.grad += self.T.dot(dy)
+
+        out = Tensor(
+            array=self.array.dot(other.array),
+            args=(self, other),
+            name="dot",
+            backward_fn=_backward,
+        )
+        return out
+
+    def relu(self) -> "Tensor":
+        def _backward(dy: "Tensor") -> None:
+            self.grad += dy * (self > 0)  # type: ignore
+
+        out = Tensor(
+            array=np.maximum(0, self.array),
+            args=(self,),
+            name="ReLU",
+            backward_fn=_backward,
+        )
+
+        return out
 
     def __hash__(self) -> int:
+        """
+        Return the hash value of the object.
+
+        Returns:
+            int: The hash value of the object.
+        """
         return self._id
 
-    def __add__(self, other: float | "Tensor") -> "Tensor":
-        """
-        Add two tensors.
-        """
-        if isinstance(other, numbers.Number):
+    def __add__(self, other: Union[int, float, "Tensor"]) -> "Tensor":
+
+        if isinstance(other, (int, float)):
             # The gradient of the number can be ignored.
+            def _backward(dy: "Tensor") -> None:
+                self.grad += dy
+
             out = Tensor(
                 array=self.array + other,
                 args=(self,),
                 name=f"+ {other}",
+                backward_fn=_backward,
             )
-
-            def _backward() -> None:
-                self.grad += out.grad
-
-            out._backward = _backward
 
         elif isinstance(other, Tensor):
             # The gradient of the tensor must be computed.
+            def _backward(dy: "Tensor") -> None:
+                self.grad += dy
+                other.grad += dy
+
             out = Tensor(
                 array=self.array + other.array,
                 args=(self, other),
                 name="+",
+                backward_fn=_backward,
             )
-
-            def _backward() -> None:
-                self.grad += out.grad
-                other.grad += out.grad
-
-            out._backward = _backward
 
         else:
             raise NotImplementedError(f"Cannot add {type(self)} and {type(other)}")
 
         return out
 
-    def __radd__(self, other: float | "Tensor") -> "Tensor":
+    def __radd__(self, other: Union[int, float, "Tensor"]) -> "Tensor":
         return self + other
 
-    def __iadd__(self, other: float | "Tensor") -> "Tensor":
+    def __iadd__(self, other: Union[int, float, "Tensor"]) -> "Tensor":
         return self + other
 
-    def __sub__(self, other: float | "Tensor") -> "Tensor":
+    def __sub__(self, other: Union[int, float, "Tensor"]) -> "Tensor":
         """
         Subtract two tensors.
         """
-        if isinstance(other, numbers.Number):
+        if isinstance(other, (int, float)):
             # The gradient of the number can be ignored.
+            def _backward(dy: "Tensor") -> None:
+                self.grad += dy
+
             out = Tensor(
                 array=self.array - other,
                 args=(self,),
                 name=f"- {other}",
+                backward_fn=_backward,
             )
 
-            def _backward() -> None:
-                self.grad += out.grad
-
-            out._backward = _backward
-
-        elif isinstance(other, "Tensor"):
+        elif isinstance(other, Tensor):
             # The gradient of the tensor must be computed.
+            def _backward(dy: "Tensor") -> None:
+                self.grad += dy
+                other.grad -= dy
+
             out = Tensor(
                 array=self.array - other.array,
                 args=(self, other),
                 name="-",
+                backward_fn=_backward,
             )
-
-            def _backward() -> None:
-                self.grad += out.grad
-                other.grad -= out.grad
-
-            out._backward = _backward
 
         else:
             raise NotImplementedError(f"Cannot subtract {type(self)} and {type(other)}")
 
         return out
 
-    def __rsub__(self, other: float | "Tensor") -> "Tensor":
-        return -self.__sub__(other)
+    def __rsub__(self, other: Union[int, float, "Tensor"]) -> "Tensor":
+        return other + (-self)
 
-    def __isub__(self, other: float | "Tensor") -> "Tensor":
-        return self.__sub__(other)
+    def __isub__(self, other: Union[int, float, "Tensor"]) -> "Tensor":
+        return self - other
 
-    def __mul__(self, other: float | "Tensor") -> "Tensor":
+    def __mul__(self, other: Union[int, float, "Tensor"]) -> "Tensor":
         """
         Multiply two tensors.
         """
 
-        if isinstance(other, numbers.Number):
+        if isinstance(other, (int, float)):
             # The gradient of the number can be ignored
+            def _backward(dy: "Tensor") -> None:
+                self.grad += other * dy
+
             out = Tensor(
                 array=self.array * other,
                 args=(self,),
                 name=f"* {other}",
+                backward_fn=_backward,
             )
 
-            def _backward() -> None:
-                self.grad += other * out.grad
-
-            out._backward = _backward
-
-        elif isinstance(other, "Tensor"):
+        elif isinstance(other, Tensor):
             # The gradient of the tensor must be computed
+            def _backward(dy: "Tensor") -> None:
+                self.grad += other * dy
+                other.grad += self * dy
+
             out = Tensor(
                 array=self.array * other.array,
                 args=(self, other),
                 name="*",
+                backward_fn=_backward,
             )
-
-            def _backward() -> None:
-                self.grad += other.array * out.grad
-                other.grad += self.array * out.grad
-
-            out._backward = _backward
 
         else:
             raise NotImplementedError(f"Cannot multiply {type(self)} and {type(other)}")
 
         return out
 
-    def __rmul__(self, other: float | "Tensor") -> "Tensor":
-        return self.__mul__(other)
+    def __rmul__(self, other: Union[int, float, "Tensor"]) -> "Tensor":
+        # Multiplication is commutative
+        return self * other
 
-    def __imul__(self, other: float | "Tensor") -> "Tensor":
-        return self.__mul__(other)
+    def __imul__(self, other: Union[int, float, "Tensor"]) -> "Tensor":
+        return self * other
 
     def __neg__(self) -> "Tensor":
-        return self * -1
+        return self * (-1.0)
 
-    def __pow__(self, other: int | float | "Tensor") -> "Tensor":
+    def __matmul__(self, other: "Tensor") -> "Tensor":
+        """
+        Matrix multiplication.
+        """
+
+        def _backward(dy: "Tensor") -> None:
+            self.grad += dy @ other.T
+            other.grad += self.T @ dy
+
+        out = Tensor(
+            array=self.array @ other.array,
+            args=(self, other),
+            name="@",
+            backward_fn=_backward,
+        )
+
+        return out
+
+    def __pow__(self, other: Union[int, float, "Tensor"]) -> "Tensor":
         """
         Exponention.
         """
 
-        if isinstance(other, numbers.Number):
+        if isinstance(other, (int, float)):
+
+            def _backward(dy: "Tensor") -> None:
+                self.grad += (other * self ** (other - 1)) * dy
+
             out = Tensor(
                 array=self.array**other,
                 args=(self,),
                 name=f"** {other}",
+                backward_fn=_backward,
             )
-
-            def _backward() -> None:
-                self.grad += (other * self.array ** (other - 1)) * out.grad
-
-            out._backward = _backward
 
         elif isinstance(other, Tensor):
             raise NotImplementedError("Exponentiation of two tensors is not supported.")
@@ -217,14 +343,105 @@ class Tensor:
 
         return out
 
-    def __rpow__(self, other: int | float | "Tensor") -> "Tensor":
-        return self.__pow__(other)
+    def __rpow__(self, other: Union[int, float, "Tensor"]) -> "Tensor":
+        raise NotImplementedError("Exponentiation is not commutative.")
 
-    def __ipow__(self, other: float | "Tensor") -> "Tensor":
-        return self.__pow__(other)
+    def __ipow__(self, other: Union[int, float, "Tensor"]) -> "Tensor":
+        return self**other
 
-    def __truediv__(self, other: float | "Tensor") -> "Tensor":
+    def __truediv__(self, other: Union[int, float, "Tensor"]) -> "Tensor":
         """
         Division.
         """
-        pass
+        if isinstance(other, (int, float)):
+            return self * (1 / other)
+
+        elif isinstance(other, Tensor):
+            return self * (other**-1)
+        else:
+            raise NotImplementedError(f"Cannot divide {type(self)} by {type(other)}")
+
+    def __rtruediv__(self, other: Union[int, float, "Tensor"]) -> "Tensor":
+        if isinstance(other, (int, float)):
+            return other * (self**-1)
+
+        elif isinstance(other, Tensor):
+            return other * (1 / self)
+
+        else:
+            raise NotImplementedError(f"Cannot divide {type(other)} by {type(self)}")
+
+    def __itruediv__(self, other: Union[int, float, "Tensor"]) -> "Tensor":
+        return self / other
+
+    def __lt__(self, other: Union[int, float, "Tensor"]) -> "Tensor":
+        """
+        Less than comparison.
+        """
+        if isinstance(other, Tensor):
+            return Tensor(self.array < other.array)
+
+        return Tensor(self.array < other)
+
+    def __le__(self, other: Union[int, float, "Tensor"]) -> "Tensor":
+        """
+        Less than or equal to comparison.
+        """
+        if isinstance(other, Tensor):
+            return Tensor(self.array <= other.array)
+
+        return Tensor(self.array <= other)
+
+    def __eq__(self, other: Union[int, float, "Tensor"]) -> "Tensor":
+        """
+        Equal to comparison.
+        """
+        if isinstance(other, Tensor):
+            if other._id == self._id:
+                return Tensor(True)
+            return Tensor(self.array == other.array)
+
+        return Tensor(self.array == other)
+
+    def __ne__(self, other: Union[int, float, "Tensor"]) -> "Tensor":
+        """
+        Not equal to comparison.
+        """
+        if isinstance(other, Tensor):
+            return Tensor(self.array != other.array)
+
+        return Tensor(self.array != other)
+
+    def __gt__(self, other: Union[int, float, "Tensor"]) -> "Tensor":
+        """
+        Greater than comparison.
+        """
+        if isinstance(other, Tensor):
+            return Tensor(self.array > other.array)
+
+        return Tensor(self.array > other)
+
+    def __ge__(self, other: Union[int, float, "Tensor"]) -> "Tensor":
+        """
+        Greater than or equal to comparison.
+        """
+        if isinstance(other, Tensor):
+            return Tensor(self.array >= other.array)
+
+        return Tensor(self.array >= other)
+
+    def __repr__(self) -> str:
+        name = f", name={self.name}" if self.name is not None else ""
+        return f"Tensor({self.array.__str__()}{name})"
+
+    def __getitem__(self, idx: Union[slice, int]) -> "Tensor":
+        # TODO: Ensure that the gradient is tracked properly
+        return Tensor(self.array[idx], requires_grad=self.requires_grad)
+
+    def __setitem__(
+        self, idx: Union[slice, int], value: Union[int, float, "Tensor"]
+    ) -> None:
+        if isinstance(value, Tensor):
+            self.array[idx] = value.array
+        else:
+            self.array[idx] = value
