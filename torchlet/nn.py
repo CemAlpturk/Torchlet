a0950ea1166a89334a314f6f7ef11d3f032563f2
Cem Alpturk <cem.alpturk@gmail.com>
2024-08-22 23:57:48 +0200

    toy problem

--- torchlet/nn.py
+++ torchlet/nn.py
@@ -39,28 +39,61 @@ class Linear(Module):
         self.b = Tensor(data=np.zeros((out_features,)))
 
     def forward(self, x: Tensor) -> Tensor:
-        z = x @ self.W
+        # z = x @ self.W
 
-        # Manually add the backward function
-        # This is due to the broadcast of the bias term
-        # Until I find a better way to do this
+        # # Manually add the backward function
+        # # This is due to the broadcast of the bias term
+        # # Until I find a better way to do this
 
-        result = z.data + self.b.data
+        # result = z.data + self.b.data
 
-        def _backward(dy: np.ndarray) -> None:
-            z.grad += dy
-            if z.shape != self.b.shape:
-                dy = dy.sum(axis=0)
-            self.b.grad += dy
+        # def _backward(dy: np.ndarray) -> None:
+        #     z.grad += dy
+        #     if z.shape != self.b.shape:
+        #         dy = dy.sum(axis=0)
+        #     self.b.grad += dy
 
-        out = Tensor(
-            data=result,
-            dtype=z.dtype,
-            name="bias_broadcast",
-            backward_fn=_backward,
-        )
+        # out = Tensor(
+        #     data=result,
+        #     dtype=z.dtype,
+        #     name="bias_broadcast",
+        #     backward_fn=_backward,
+        # )
 
-        return out
+        # return out
+
+        return x @ self.W + self.b
 
     def parameters(self) -> list[Tensor]:
         return [self.W, self.b]
+
+    def __repr__(self) -> str:
+        return f"Linear({self.W.data.shape[0]}, {self.W.data.shape[1]})"
+
+
+class ReLU(Module):
+
+    def forward(self, x: Tensor) -> Tensor:
+        return x.relu()
+
+    def __repr__(self) -> str:
+        return "ReLU()"
+
+
+class Sequential(Module):
+    def __init__(self, *args: Module) -> None:
+        self.modules = args
+
+    def forward(self, x: Tensor) -> Tensor:
+        for module in self.modules:
+            x = module(x)
+        return x
+
+    def parameters(self) -> list[Tensor]:
+        params = []
+        for module in self.modules:
+            params.extend(module.parameters())
+        return params
+
+    def __repr__(self) -> str:
+        return f"Sequential({', '.join([str(module) for module in self.modules])})"
