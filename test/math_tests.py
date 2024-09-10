5ca22946914f39f94eb7085b29eebc33832f5327
Cem Alpturk <cem.alpturk@gmail.com>
2024-09-10 09:20:19 +0300

    complete tensor tests

---
+++ test/math_tests.py
@@ -0,0 +1,207 @@
+# type: ignore
+
+from typing import Callable, Generic, Iterable, TypeVar
+
+from torchlet import scalar_ops as ops
+
+A = TypeVar("A")
+
+
+class MathTest(Generic[A]):
+
+    @staticmethod
+    def neg(a: A) -> A:
+        """Negate the argument"""
+        return -a
+
+    @staticmethod
+    def addConstant(a: A) -> A:
+        """Add a constant to the argument"""
+        return 5 + a
+
+    @staticmethod
+    def square(a: A) -> A:
+        """Manual square"""
+        return a * a
+
+    @staticmethod
+    def cube(a: A) -> A:
+        """Manual cube"""
+        return a * a * a
+
+    @staticmethod
+    def subConstant(a: A) -> A:
+        """Subtract a constant from the argument"""
+        return a - 5
+
+    @staticmethod
+    def multiConstant(a: A) -> A:
+        """Multiply a constant to the argument"""
+        return 5 * a
+
+    @staticmethod
+    def div(a: A) -> A:
+        """Divide by a constant"""
+        return a / 5
+
+    @staticmethod
+    def inv(a: A) -> A:
+        """Invert after adding"""
+        return ops.inv(a + 3.5)
+
+    @staticmethod
+    def sig(a: A) -> A:
+        """Apply sigmoid"""
+        return ops.sigmoid(a)
+
+    @staticmethod
+    def log(a: A) -> A:
+        """Apply log to a large value"""
+        return ops.log(a + 100000)
+
+    @staticmethod
+    def relu(a: A) -> A:
+        """Apply relu"""
+        return ops.relu(a + 5.5)
+
+    @staticmethod
+    def exp(a: A) -> A:
+        """Apply exp to a smaller value"""
+        return ops.exp(a - 200)
+
+    @staticmethod
+    def explog(a: A) -> A:
+        return ops.log(a + 100000) + ops.exp(a - 200)
+
+    @staticmethod
+    def add2(a: A, b: A) -> A:
+        """Add two arguments"""
+        return a + b
+
+    @staticmethod
+    def mul2(a: A, b: A) -> A:
+        """Multiply two arguments"""
+        return a * b
+
+    @staticmethod
+    def div2(a: A, b: A) -> A:
+        """Divide two arguments"""
+        return a / (b + 5.5)
+
+    @staticmethod
+    def gt2(a: A, b: A) -> A:
+        return ops.lt(b, a + 1.2)
+
+    @staticmethod
+    def lt2(a: A, b: A) -> A:
+        return ops.lt(a + 1.2, b)
+
+    @staticmethod
+    def eq2(a: A, b: A) -> A:
+        return ops.eq(a, (b + 5.5))
+
+    @staticmethod
+    def sum_red(a: Iterable[A]) -> A:
+        return ops.sum(a)
+
+    @staticmethod
+    def mean_red(a: Iterable[A]) -> A:
+        return ops.sum(a) / float(len(a))
+
+    @staticmethod
+    def mean_full_red(a: Iterable[A]) -> A:
+        return ops.sum(a) / float(len(a))
+
+    @staticmethod
+    def complex(a: A) -> A:
+        return ops.log(ops.sigmoid(ops.relu(ops.relu(a * 10 + 7) * 6 + 5) * 10)) / 50
+
+    @classmethod
+    def _tests(cls) -> tuple[
+        tuple[str, Callable[[A], A]],
+        tuple[str, Callable[[A, A], A]],
+        tuple[str, Callable[[Iterable[A]], A]],
+    ]:
+        """
+        Returns a list of all the math tests.
+        """
+
+        one_arg = []
+        two_arg = []
+        red_arg = []
+        for k in dir(MathTest):
+            if callable(getattr(MathTest, k)) and not k.startswith("_"):
+                base_fn = getattr(cls, k)
+                tup = (k, base_fn)
+                if k.endswith("2"):
+                    two_arg.append(tup)
+                elif k.endswith("red"):
+                    red_arg.append(tup)
+                else:
+                    one_arg.append(tup)
+
+        return one_arg, two_arg, red_arg
+
+    @classmethod
+    def _comp_testing(cls):
+        one_arg, two_arg, red_arg = cls._tests()
+        one_argv, two_argv, red_argv = MathTest._tests()
+        one_arg = [(n1, f2, f1) for (n1, f1), (n2, f2) in zip(one_arg, one_argv)]
+        two_arg = [(n1, f2, f1) for (n1, f1), (n2, f2) in zip(two_arg, two_argv)]
+        red_arg = [(n1, f2, f1) for (n1, f1), (n2, f2) in zip(red_arg, red_argv)]
+        return one_arg, two_arg, red_arg
+
+
+class MathTestVariable(MathTest):
+
+    @staticmethod
+    def inv(a):
+        return 1.0 / (a + 3.5)
+
+    @staticmethod
+    def sig(x):
+        return x.sigmoid()
+
+    @staticmethod
+    def log(x):
+        return (x + 100000).log()
+
+    @staticmethod
+    def relu(x):
+        return (x + 5.5).relu()
+
+    @staticmethod
+    def exp(a):
+        return (a - 200).exp()
+
+    @staticmethod
+    def explog(a):
+        return (a + 100000).log() + (a - 200).exp()
+
+    @staticmethod
+    def sum_red(a):
+        return a.sum(0)
+
+    @staticmethod
+    def mean_red(a):
+        return a.mean(0)
+
+    @staticmethod
+    def mean_full_red(a):
+        return a.mean()
+
+    @staticmethod
+    def eq2(a, b):
+        return a == (b + 5.5)
+
+    @staticmethod
+    def gt2(a, b):
+        return a + 1.2 > b
+
+    @staticmethod
+    def lt2(a, b):
+        return a + 1.2 < b
+
+    @staticmethod
+    def complex(a):
+        return (((a * 10 + 7).relu() * 6 + 5).relu() * 10).sigmoid().log() / 50
---
+++ test/test_tensor.py
@@ -0,0 +1,204 @@
+from typing import Callable, Iterable
+
+import pytest
+from hypothesis import given
+from hypothesis.strategies import DataObject, data, lists, permutations
+
+from torchlet import Tensor, tensor
+from torchlet.tensor_functions import grad_check
+
+from .strategies import assert_close, small_floats
+from .tensor_strategies import shaped_tensors, tensors
+from .math_tests import MathTestVariable
+
+one_arg, two_arg, red_arg = MathTestVariable._comp_testing()
+
+
+@given(lists(small_floats, min_size=1))
+def test_create(t1: list[float]) -> None:
+    """Test the ability to create an index a 1D Tensor"""
+    t2 = tensor(t1)
+    for i in range(len(t1)):
+        assert t1[i] == t2[i]
+
+
+@given(tensors())
+@pytest.mark.parametrize("fn", one_arg)
+def test_one_args(
+    fn: tuple[str, Callable[[float], float], Callable[[Tensor], Tensor]],
+    t1: Tensor,
+) -> None:
+    """Test one-arg functions compared to floats"""
+    name, base_fn, tensor_fn = fn
+    t2 = tensor_fn(t1)
+    for ind in t2._tensor.indices():
+        assert_close(t2[ind], base_fn(t1[ind]))
+
+
+@given(shaped_tensors(2))
+@pytest.mark.parametrize("fn", two_arg)
+def test_two_args(
+    fn: tuple[str, Callable[[float, float], float], Callable[[Tensor, Tensor], Tensor]],
+    ts: tuple[Tensor, Tensor],
+) -> None:
+    name, base_fn, tensor_fn = fn
+    t1, t2 = ts
+    t3 = tensor_fn(t1, t2)
+    for ind in t3._tensor.indices():
+        assert_close(t3[ind], base_fn(t1[ind], t2[ind]))
+
+
+@given(tensors(requires_grad=True))
+@pytest.mark.parametrize("fn", one_arg)
+def test_one_derivative(
+    fn: tuple[str, Callable[[float], float], Callable[[Tensor], Tensor]],
+    t1: Tensor,
+) -> None:
+    """Test the gradient of a none-arg tensor function"""
+    name, _, tensor_fn = fn
+    grad_check(tensor_fn, t1)
+
+
+@given(data(), tensors())
+def test_permute(data: DataObject, t1: Tensor) -> None:
+    """Test permute function"""
+    permutation = data.draw(permutations(range(len(t1.shape))))
+
+    def permute(a: Tensor) -> Tensor:
+        return a.permute(*permutation)
+
+    grad_check(permute, t1)
+
+
+def test_grad_size() -> None:
+    """Test the size of the gradient"""
+    a = tensor([1], requires_grad=True)
+    b = tensor([[1, 1]], requires_grad=True)
+
+    c = (a * b).sum()
+    c.backward()
+
+    assert c.shape == (1,)
+    assert a.grad is not None
+    assert b.grad is not None
+    assert a.shape == a.grad.shape
+    assert b.shape == b.grad.shape
+
+
+@given(tensors())
+@pytest.mark.parametrize("fn", red_arg)
+def test_grad_reduce(
+    fn: tuple[str, Callable[[Iterable[float]], float], Callable[[Tensor], Tensor]],
+    t1: Tensor,
+) -> None:
+    """Test the grad of a tensor reduce"""
+    name, _, tensor_fn = fn
+    grad_check(tensor_fn, t1)
+
+
+@given(shaped_tensors(2))
+@pytest.mark.parametrize("fn", two_arg)
+def test_two_grad(
+    fn: tuple[str, Callable[[float], float], Callable[[Tensor, Tensor], Tensor]],
+    ts: tuple[Tensor, Tensor],
+) -> None:
+    name, _, tensor_fn = fn
+    t1, t2 = ts
+    grad_check(tensor_fn, t1, t2)
+
+
+@given(shaped_tensors(2))
+@pytest.mark.parametrize("fn", two_arg)
+def test_two_grad_broadcast(
+    fn: tuple[str, Callable[[float, float], float], Callable[[Tensor, Tensor], Tensor]],
+    ts: tuple[Tensor, Tensor],
+) -> None:
+    """Test the grad of a two argument function"""
+    _, _, tensor_fn = fn
+    t1, t2 = ts
+    grad_check(tensor_fn, t1, t2)
+
+    t11 = t1.sum(0).detach()
+    t12 = t2.detach()
+
+    t21 = t1.detach()
+    t22 = t2.sum(0).detach()
+
+    # broadcast check
+    grad_check(tensor_fn, t11, t12)
+    grad_check(tensor_fn, t21, t22)
+
+
+def test_fromList() -> None:
+    """Test longer from list conversion"""
+    t = tensor([[2, 3, 4], [4, 5, 7]])
+    assert t.shape == (2, 3)
+
+    t = tensor([[[2, 3, 4], [4, 5, 7]]])
+    assert t.shape == (1, 2, 3)
+
+
+def test_view() -> None:
+    """Test view"""
+    t = tensor([[2, 3, 4], [4, 5, 7]])
+    assert t.shape == (2, 3)
+    t2 = t.view(6)
+    assert t2.shape == (6,)
+    t2 = t2.view(1, 6)
+    assert t2.shape == (1, 6)
+    t2 = t2.view(6, 1)
+    assert t2.shape == (6, 1)
+    t2 = t2.view(2, 3)
+    assert t.is_close(t2).all().item() == 1.0
+
+
+@given(tensors())
+def test_back_view(t1: Tensor) -> None:
+    """Test the gradient of view"""
+
+    def view(a: Tensor) -> Tensor:
+        a = a.contiguous()
+        return a.view(a.size)
+
+    grad_check(view, t1)
+
+
+@pytest.mark.xfail
+def test_permute_view() -> None:
+    t = tensor([[2, 3, 4], [4, 5, 7]])
+    assert t.shape == (2, 3)
+    t2 = t.permute(1, 0)
+    t2.view(6)
+
+
+@pytest.mark.xfail
+def test_index() -> None:
+    t = tensor([[2, 3, 4], [4, 5, 7]])
+    assert t.shape == (2, 3)
+    t[50, 2]
+
+
+def test_fromnumpy() -> None:
+    t = tensor([[2, 3, 4], [4, 5, 7]])
+    assert t.shape == (2, 3)
+    n = t.to_numpy()
+    t2 = tensor(n.tolist())
+    for ind in t._tensor.indices():
+        assert t[ind] == t2[ind]
+
+
+def test_reduce_forward_one_dim() -> None:
+    t = tensor([[2, 3], [4, 6], [5, 7]])
+
+    t_summed = t.sum(0)
+
+    t_sum_expected = tensor([[11, 16]])
+
+    assert t_summed.is_close(t_sum_expected).all().item()
+
+
+def test_reduce_forward_all_dims() -> None:
+    t = tensor([[2, 3], [4, 6], [5, 7]])
+    t_summed_all = t.sum()
+    t_summed_all_expected = tensor([27])
+    assert_close(t_summed_all[0], t_summed_all_expected[0])
